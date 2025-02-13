c5d7bd2ec5d2d594e01639bf8c781a9fba5d8467
王勇 <wangyong@wangyongdeMacBook-Pro.local>
2025-02-13 10:46:45 +0800

    2025-02-13

---
+++ docs/.vuepress/theme/layouts/GlobalLayout.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="global-layout">
+      <component :is="layout"/>
+      <footer>
+        <div style="text-align: center;">
+            <a href="https://beian.miit.gov.cn/" target="_blank">备案号:浙ICP备2021001694号-2</a>
+        </div>
+      </footer>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    computed: {
+      layout () {
+        if (this.$page.path) {
+          if (this.$frontmatter.layout) {
+            // 你也可以像默认的 globalLayout 一样首先检测 layout 是否存在
+            return this.$frontmatter.layout
+          }
+          return 'Layout'
+        }
+        return 'NotFound'
+      }
+    }
+  }
+  </script>
